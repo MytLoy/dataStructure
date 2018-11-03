69e8f4f6f67fec944b52e5e7d961b31a7e55040c
linyiping <123@qq.com>
2018-11-03 18:51:41 +0800

    循环队列

---
+++ Queue/src/LoppQueue.java
@@ -0,0 +1,104 @@
+public class LoppQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    // 构造函数
+    public LoppQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1]; // 循环队列会有意浪费一个位置
+    }
+
+    public  LoppQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    // 入队
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) { // 队满
+            resize(getCapacity() * 2);
+        }
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
+    }
+
+    @Override
+    // 出队
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue");
+        }
+        E ret = data[front];
+        data[front] = null; // 因为是引用，防止浪费
+        front = (front + 1) % data.length;
+        size --;
+        // 进行缩容，缩容不能等于0
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty");
+        }
+        return data[front];
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length]; // 循环
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue:size = %d , capacity = %d\n", size, getCapacity()));
+        res.append("front [");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            res.append(data[i]);
+            if ((i +  1) % data.length != tail) {
+                res.append(", ");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LoppQueue<Integer> queue = new LoppQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+
+}
--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class Main {
 
+    // 测试入队和出队时间
+    private static double testQueue(Queue<Integer>q, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        int opCount = 10000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoppQueue<Integer> loppQueue = new LoppQueue<>();
+        double time2 = testQueue(loppQueue, opCount);
+        System.out.println("LoppQueue, time: " + time2 + " s");
     }
 }
